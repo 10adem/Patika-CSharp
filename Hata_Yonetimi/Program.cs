7a63ce900e2d77da092b9eedd21e524e847c48fe
Adem Yıldız <ademy0783@gmail.com>
2022-12-18 11:12:16 +0300

    Error Management - Try Catch Finally

---
+++ Hata_Yonetimi/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Hata_Yonetimi;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Bir sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş Olduğunuz Sayı : " + sayi);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata : " + ex.Message.ToString());
+        }
+        // finally
+        // {
+        //     Console.WriteLine("İşlem Tamamlandı.");
+        // }
+
+
+        try
+        {
+            int a = int.Parse(null);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Boş Değer Girdiniz!");
+            Console.WriteLine(ex);
+        }
+
+
+        try
+        {
+            int a = int.Parse("test");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Veri Tipi Uygun Değil!");
+            Console.WriteLine(ex);
+        }
+
+
+        try
+        {
+            int a = int.Parse("-2000000000");
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Çok Küçük ya da Çok Büyük Bir Değer Girdiniz!");
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            Console.WriteLine("İşlem Başarıyla Tamamlandı.");
+        }
+    }
+}
